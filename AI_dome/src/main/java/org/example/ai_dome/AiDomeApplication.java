11226ef3a529d589e357ab39451af8bf2454f4d5
Luoxing7 <3296874694@qq.com>
2026-05-25 23:27:49 +0800

    使用Spring boot 对接deepseek

---
+++ AI_dome/src/main/java/org/example/ai_dome/AiDomeApplication.java
@@ -0,0 +1,13 @@
+package org.example.ai_dome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiDomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiDomeApplication.class, args);
+    }
+
+}
---
+++ AI_dome/src/test/java/org/example/ai_dome/AiDomeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ai_dome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiDomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
